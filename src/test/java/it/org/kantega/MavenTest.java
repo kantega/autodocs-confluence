7f2269a995532a80d5e688384f307317cc846428
Atle Prange <atle.prange@gmail.com>
2016-07-01 09:10:44 +0200

    Fix test

--- src/test/java/it/org/kantega/MavenTest.java
+++ src/test/java/it/org/kantega/MavenTest.java
@@ -6,6 +6,7 @@ import fj.data.Validation;
 import org.junit.Test;
 import org.kantega.documenter.MavenDocumentationLocator;
 import org.kantega.documenter.api.DocumentationLocator;
+import org.kantega.documenter.api.FailedPluginDoc;
 import org.kantega.documenter.api.PluginDoc;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -17,9 +18,9 @@ public class MavenTest {
         DocumentationLocator locator = new MavenDocumentationLocator();
 
 
-        final Validation<String, List<Either<String, PluginDoc>>> documentationFor =
+        final Validation<String, List<Either<FailedPluginDoc, PluginDoc>>> documentationFor =
           locator.getDocumentationFor("no.nte.distributions.erp:erp-handler:1.8-SNAPSHOT");
         documentationFor.f().forEach(System.out::println);
-        documentationFor.forEach(eithers -> eithers.forEach(either->System.out.println(either.<String>either(f->f,s->s.documentRoot.toString()))));
+        documentationFor.forEach(eithers -> eithers.forEach(either->System.out.println(either.<String>either(Object::toString,s->s.documentRoot.toString()))));
     }
 }
